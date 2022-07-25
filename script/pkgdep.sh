70fd4c74a446847dfe24576e06100d3ff6fb9118
Junghyun Hong <jh34.hong@samsung.com>
2022-07-25 04:28:05 +0000

    [AWIBOF-6677] Add RAID 6 to POS array fault-tolerance.
    Add RAID 6 class and options to support POS array fault-tolerance.
    
    Add ISA-L packages to pkgdep.sh, Makefile, and package control file for RAID 6 support.
    
    Signed-off-by: Junghyun Hong <jh34.hong@samsung.com>

--- script/pkgdep.sh
+++ script/pkgdep.sh
@@ -70,6 +70,8 @@ if [ -f /etc/debian_version ]; then
     apt install -y libgoogle-perftools-dev
     # for rocksdb
     apt install -y librocksdb-dev
+    # for isal
+    apt install -y libisal-dev
 
 
 else
